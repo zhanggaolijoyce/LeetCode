2e7aa613c8de77cdafa4babeb3a7dfaad48ccc6c
zhanggaolijoyce <130479536+zhanggaolijoyce@users.noreply.github.com>
2024-07-27 16:33:55 +0800

    Create 88. 合并两个有序数组.java

---
+++ Java/88. 合并两个有序数组.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = nums1.length -1;
+        int i = m -1;
+        int j = n -1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j]) {
+                nums1[k] = nums1[i];
+                i --;
+            } else {
+                nums1[k] = nums2[j];
+                j --;
+            }
+            k --;
+        }
+        if (i == -1) {
+            while (j >= 0) {
+                nums1[j] = nums2[j];
+                j --;
+            }
+        }
+    }
+}
